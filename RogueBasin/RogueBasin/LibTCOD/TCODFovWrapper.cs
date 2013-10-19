63fa02d21d52a2644ed181fdf241e60d0aa05c92
flend <seramygames@gmail.com>
2013-10-19 15:51:13 +0100

    Missing file

---
+++ RogueBasin/RogueBasin/LibTCOD/TCODFovWrapper.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using libtcodWrapper;
+
+namespace RogueBasin.LibTCOD
+{
+    class TCODFovWrapper
+    {
+        Dictionary<int, TCODFov> levelTCODMaps;
+
+        public TCODFovWrapper()
+        {
+            levelTCODMaps = new Dictionary<int, TCODFov>();
+        }
+
+        public void addMap(int level, int width, int height)
+        {
+            levelTCODMaps[level] = new TCODFov(width, height);
+        }
+
+        public TCODFov getMap(int level)
+        {
+            return levelTCODMaps[level];
+        }
+    }
+}
