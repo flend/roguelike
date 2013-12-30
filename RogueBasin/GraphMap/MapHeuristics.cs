6236d854429a2efc50abf44c01f44656850ae48a
flend <seramygames@gmail.com>
2013-12-30 23:26:00 +0000

    Some simple heuristics

---
+++ RogueBasin/GraphMap/MapHeuristics.cs
@@ -0,0 +1,46 @@
+﻿using QuickGraph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphMap
+{
+    /** Carries out useful heuristics on reduced maps */
+    public class MapHeuristics
+    {
+        public readonly MapCycleReducer mapWithoutCycles;
+        public readonly int startVertex;
+
+        public MapHeuristics(MapCycleReducer mapWithoutCycles, int startVertex)
+        {
+            this.mapWithoutCycles = mapWithoutCycles;
+            this.startVertex = startVertex;
+        }
+
+        public IEnumerable<int> GetNodesOnStartSideOfConnection(Connection dividingConnection) {
+            
+            //Check the edge is in the reduced map (will throw an exception if can't find)
+            var foundEdge = mapWithoutCycles.GetEdgeBetweenRoomsNoCycles(dividingConnection.Source, dividingConnection.Target);
+
+            //Remove all areas behind the locked door
+            MapSplitter allowedMap = new MapSplitter(mapWithoutCycles.mapNoCycles.Edges, foundEdge, startVertex);
+
+            //Find the component of this broken graph that is connected to the start vertex - 
+            //This component contains the vertices accessible with these clues
+            return allowedMap.MapComponent(allowedMap.RoomComponentIndex(startVertex));
+        }
+
+
+        /** Get nodes depth nodes away from leaves (dead ends) */
+        public IEnumerable<int> GetTerminalBranchNodes(int depth)
+        {
+            if(depth > 0)
+                throw new NotImplementedException();
+
+            var graphMap = mapWithoutCycles.mapNoCycles;
+            return graphMap.Vertices.Where(v => graphMap.AdjacentEdges(v).Count() == 1);
+        }
+    }
+}
---
+++ RogueBasin/TestGraphMap/MapHeuristicsTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GraphMap;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestGraphMap
+{
+    [TestClass]
+    public class MapHeuristicsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void NodesOnSideOfConnectionCanBeFound()
+        {
+            MapHeuristics mapH = BuildMapHeuristics();
+
+            var accessibleNodes = mapH.GetNodesOnStartSideOfConnection(new Connection(2, 10));
+            CollectionAssert.AreEquivalent(new List<int>(new int[] { 1, 2, 3, 4, 5, 6 }), accessibleNodes.ToList());
+        }
+
+        [TestMethod]
+        public void DeadEndFinalRoomsCanBeFound()
+        {
+
+            MapHeuristics mapH = BuildMapHeuristics();
+            var deadEndNodes = mapH.GetTerminalBranchNodes(0).ToList();
+
+            CollectionAssert.AreEquivalent(new List<int>(new int[] { 1, 4, 6, 12, 14, 15 }), deadEndNodes);
+        }
+
+        private MapHeuristics BuildMapHeuristics()
+        {
+            var standardMap = BuildStandardTestMap();
+            var mapNoCycles = new MapCycleReducer(standardMap.RoomConnectionGraph.Edges);
+            return new MapHeuristics(mapNoCycles, 1);
+        }
+
+        private ConnectivityMap BuildStandardTestMap()
+        {
+            ConnectivityMap newMap = new ConnectivityMap();
+
+            newMap.AddRoomConnection(1, 2);
+            newMap.AddRoomConnection(2, 3);
+
+            //Branch
+
+            newMap.AddRoomConnection(3, 4);
+            newMap.AddRoomConnection(3, 5);
+            newMap.AddRoomConnection(5, 6);
+
+            //Cycle
+
+            newMap.AddRoomConnection(2, 8);
+            newMap.AddRoomConnection(2, 7);
+            newMap.AddRoomConnection(8, 9);
+            newMap.AddRoomConnection(7, 9);
+
+            //Post-cycle
+
+            newMap.AddRoomConnection(9, 10);
+            newMap.AddRoomConnection(10, 11);
+            newMap.AddRoomConnection(11, 12);
+            newMap.AddRoomConnection(11, 13);
+
+            //2-way branches
+            newMap.AddRoomConnection(13, 14);
+            newMap.AddRoomConnection(13, 15);
+
+            //Save to disk
+            GraphvizExport.OutputUndirectedGraph(newMap.RoomConnectionGraph, "door-and-clue-manager-map");
+
+            return newMap;
+        }
+    }
+}
