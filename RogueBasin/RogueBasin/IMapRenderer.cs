5e12d6f2c85564884bebe9604283d7efd823dc27
flend <flend@compsoc.net>
2015-03-01 22:09:48 +0000

    Missing files

---
+++ RogueBasin/RogueBasin/IMapRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace RogueBasin
+{
+    interface IMapRenderer
+    {
+        void RenderMap(TileEngine.TileMap mapToRender, Point mapOffset, Rectangle screenViewport);
+
+        void Sleep(ulong milliseconds);
+
+        void Setup(int width, int height);
+
+        void Flush();
+
+        void Clear();
+
+        void DrawFrame(int x, int y, int width, int height, bool clear, Color color);
+
+        void PutChar(int x, int y, char c, Color color);
+
+        void PrintStringRect(string msg, int x, int y, int width, int height, libtcodWrapper.LineAlignment alignment, Color color);
+
+        void PrintString(string msg, int x, int y, libtcodWrapper.LineAlignment alignment, Color color);
+
+        void ClearRect(int x, int y, int width, int height);
+    }
+}
---
+++ RogueBasin/RogueBasin/MapRendererSDLDotNet.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using SdlDotNet.Graphics;
+using SdlDotNet.Core;
+
+
+namespace RogueBasin
+{
+    /// <summary>
+    /// Renders a multi-layer tile map onto the screen
+    /// </summary>
+    class MapRendererSDLDotNet : IMapRenderer
+    {
+
+        private Surface m_VideoScreen;
+        private Surface m_Background;
+        private Surface m_Foreground;
+        private System.Drawing.Point m_ForegroundPosition;
+
+        /// <summary>
+        /// Render the map, with TL in map at mapOffset. Screenviewport is the screen viewport in tile dimensions (for now)
+        /// </summary>
+        /// <param name="mapToRender"></param>
+        /// <param name="mapOffset"></param>
+        /// <param name="screenViewport"></param>
+        public void RenderMap(TileEngine.TileMap mapToRender, Point mapOffset, Rectangle screenViewport)
+        {
+
+        }
+
+        public void Sleep(ulong milliseconds)
+        {
+        }
+
+        public void Setup(int width, int height)
+        {
+
+            m_VideoScreen = Video.SetVideoMode(320, 240, 32, false, false, false, true);
+
+            m_Background = (new Surface(@"DemoBackground.png")).Convert(m_VideoScreen, true, false);
+            m_Foreground = (new Surface(@"DemoForeground.png")).Convert(m_VideoScreen, true, false);
+            m_Foreground.Transparent = true;
+            m_Foreground.TransparentColor = System.Drawing.Color.FromArgb(255, 0, 255);
+            m_ForegroundPosition = new System.Drawing.Point(m_VideoScreen.Width / 2 - m_Foreground.Width / 2,
+                                              m_VideoScreen.Height / 2 - m_Foreground.Height / 2);
+
+            Events.Quit += new EventHandler<QuitEventArgs>(ApplicationQuitEventHandler);
+            Events.Tick += new EventHandler<TickEventArgs>(ApplicationTickEventHandler);
+            Events.Run();
+        }
+
+        private void ApplicationTickEventHandler(object sender, TickEventArgs args)
+        {
+            m_VideoScreen.Blit(m_Background);
+            m_VideoScreen.Blit(m_Foreground, m_ForegroundPosition);
+            m_VideoScreen.Update();
+        }
+
+        private void ApplicationQuitEventHandler(object sender, QuitEventArgs args)
+        {
+            Events.QuitApplication();
+        }
+
+        public void Flush()
+        {
+
+        }
+
+        public void Clear()
+        {
+
+        }
+
+        public void DrawFrame(int x, int y, int width, int height, bool clear, Color color)
+        {
+
+        }
+
+        public void PutChar(int x, int y, char c, Color color)
+        {
+
+        }
+
+        public void PrintStringRect(string msg, int x, int y, int width, int height, libtcodWrapper.LineAlignment alignment, Color color)
+        {
+
+        }
+
+        public void PrintString(string msg, int x, int y, libtcodWrapper.LineAlignment alignment, Color color)
+        {
+
+        }
+
+        public void ClearRect(int x, int y, int width, int height)
+        {
+
+        }
+    }
+}
