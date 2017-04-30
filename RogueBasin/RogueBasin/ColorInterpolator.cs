459f17f02a99ba9f88046e9fd0d303eb638798e1
flend <flend@compsoc.net>
2017-04-30 08:38:20 +0100

    Removing libtcod from solution and cleaning up using

---
+++ RogueBasin/RogueBasin/ColorInterpolator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+
+namespace RogueBasin
+{
+    class ColorInterpolator
+    {
+        delegate byte ComponentSelector(Color color);
+        static ComponentSelector _redSelector = color => color.R;
+        static ComponentSelector _greenSelector = color => color.G;
+        static ComponentSelector _blueSelector = color => color.B;
+
+        public static Color InterpolateBetween(
+            Color endPoint1,
+            Color endPoint2,
+            double lambda)
+        {
+            if (lambda < 0 || lambda > 1)
+            {
+                throw new ArgumentOutOfRangeException("lambda");
+            }
+            System.Drawing.Color color = Color.FromArgb(
+                InterpolateComponent(endPoint1, endPoint2, lambda, _redSelector),
+                InterpolateComponent(endPoint1, endPoint2, lambda, _greenSelector),
+                InterpolateComponent(endPoint1, endPoint2, lambda, _blueSelector)
+            );
+
+            return color;
+        }
+
+        static byte InterpolateComponent(
+            Color endPoint1,
+            Color endPoint2,
+            double lambda,
+            ComponentSelector selector)
+        {
+            return (byte)(selector(endPoint1)
+                + (selector(endPoint2) - selector(endPoint1)) * lambda);
+        }
+    }
+}
